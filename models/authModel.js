38a6563b14fe28fa8adf4af61aa75d51cdbc28af
difes77 <139240487+difes77@users.noreply.github.com>
2026-01-05 23:30:50 +0700

    Update authModel.js

--- models/authModel.js
+++ models/authModel.js
@@ -7,9 +7,9 @@ exports.findUserByEmail = async (email) => {
       u.user_id, u.email, u.password, u.role,
       k.id_karyawan, k.nama_karyawan, k.id_cabang, k.no_wa,
       c.nama_cabang
-    FROM User u
-    JOIN Karyawan k ON u.id_karyawan = k.id_karyawan
-    JOIN Cabang c ON k.id_cabang = c.id_cabang
+    FROM user u
+    JOIN karyawan k ON u.id_karyawan = k.id_karyawan
+    JOIN cabang c ON k.id_cabang = c.id_cabang
     WHERE u.email = ?
   `,
     [email]
@@ -20,7 +20,7 @@ exports.findUserByEmail = async (email) => {
 exports.saveDeviceToken = async (user_id, token) => {
   await db.execute(
     `
-    INSERT INTO DeviceToken (user_id, token)
+    INSERT INTO devicetoken (user_id, token)
     VALUES (?, ?)
     ON DUPLICATE KEY UPDATE token = VALUES(token)
   `,
@@ -33,7 +33,7 @@ exports.findOwnerByEmail = async (email) => {
     `
     SELECT 
       user_id, email, password, role
-    FROM User
+    FROM user
     WHERE email = ? AND role = 'owner'
   `,
     [email]
@@ -43,14 +43,14 @@ exports.findOwnerByEmail = async (email) => {
 
 exports.createUser = async ({ email, password, role, id_karyawan }) => {
   await db.execute(
-    `INSERT INTO User (email, password, role, id_karyawan)
+    `INSERT INTO user (email, password, role, id_karyawan)
      VALUES (?, ?, ?, ?)`,
     [email, password, role, id_karyawan]
   );
 };
 
 exports.findUserByKaryawanId = async (id_karyawan) => {
-  const [rows] = await db.execute(`SELECT * FROM User WHERE id_karyawan = ?`, [
+  const [rows] = await db.execute(`SELECT * FROM user WHERE id_karyawan = ?`, [
     id_karyawan,
   ]);
   return rows[0];
