10bf581f814782decd7b02e6734254e88fa1f361
difes77 <139240487+difes77@users.noreply.github.com>
2026-01-06 01:45:51 +0700

    Update server.js

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
-app.use("/uploads", express.static("uploads"));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 // ═══════════════════════════════════════════════════════════
 // ROUTES
