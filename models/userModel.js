1a3a13956607fd6af310b614d396724c17cd3edd
difes77 <139240487+difes77@users.noreply.github.com>
2026-01-05 23:41:02 +0700

    Change SQL table names from User to user

--- models/userModel.js
+++ models/userModel.js
@@ -3,25 +3,25 @@ const db = require("../config/db");
 exports.getAllUsers = async () => {
   const [rows] = await db.execute(`
     SELECT u.user_id, u.email, u.role, k.nama_karyawan
-    FROM User u
-    LEFT JOIN Karyawan k ON u.id_karyawan = k.id_karyawan
+    FROM user u
+    LEFT JOIN karyawan k ON u.id_karyawan = k.id_karyawan
   `);
   return rows;
 };
 
 exports.deleteUser = async (id) => {
-  await db.execute(`DELETE FROM User WHERE user_id = ?`, [id]);
+  await db.execute(`DELETE FROM user WHERE user_id = ?`, [id]);
 };
 
 exports.updateUser = async (id, { email, password }) => {
   if (password) {
     await db.execute(
-      `UPDATE User SET email = ?, password = ?, updated_at = NOW() WHERE user_id = ?`,
+      `UPDATE user SET email = ?, password = ?, updated_at = NOW() WHERE user_id = ?`,
       [email, password, id]
     );
   } else {
     await db.execute(
-      `UPDATE User SET email = ?, updated_at = NOW() WHERE user_id = ?`,
+      `UPDATE user SET email = ?, updated_at = NOW() WHERE user_id = ?`,
       [email, id]
     );
   }
