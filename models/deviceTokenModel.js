12e3ef9884729432843d9df41627176011bca3dc
difes77 <139240487+difes77@users.noreply.github.com>
2026-01-05 23:33:24 +0700

    Fix table name case in device token query

--- models/deviceTokenModel.js
+++ models/deviceTokenModel.js
@@ -2,7 +2,7 @@ const db = require("../config/db");
 
 const insertOrUpdateDeviceToken = async (userId, token) => {
   const query = `
-    INSERT INTO DeviceToken (user_id, token)
+    INSERT INTO devicetoken (user_id, token)
     VALUES (?, ?)
     ON DUPLICATE KEY UPDATE token = VALUES(token), updated_at = CURRENT_TIMESTAMP
   `;
